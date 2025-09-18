0ede8fbca90e5df5fd493e48eb7f9c52db1fb866
miclol <miclam526@gmail.com>
2025-09-18 01:57:10 -0400

    tspmo

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -17,20 +17,10 @@ reg en;
 reg [3:0] count = 15;
 reg [15:0] data;
 
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        en_reg_out_7_0 <= 0;
-        en_reg_out_15_8 <= 0;
-        en_reg_pwm_7_0 <= 0;
-        en_reg_pwm_15_8 <= 0;
-        pwm_duty_cycle <= 0;
-        en <= 0;
-        count = 15;
-    end else begin
-        sh_copi <= {sh_copi[0], copi};
-        sh_ncs <= {sh_ncs[0], ncs};
-        sh_sclk <= {sh_sclk[0], sclk};
-    end
+always @(posedge clk) begin
+    sh_copi <= {sh_copi[0], copi};
+    sh_ncs <= {sh_ncs[0], ncs};
+    sh_sclk <= {sh_sclk[0], sclk};
 end
 
 assign sy_copi = sh_copi[1];
@@ -47,6 +37,15 @@ always @(posedge sy_ncs) begin
 end
 
 always @(posedge sy_sclk) begin
+    if (!rst_n) begin
+        en_reg_out_7_0 <= 0;
+        en_reg_out_15_8 <= 0;
+        en_reg_pwm_7_0 <= 0;
+        en_reg_pwm_15_8 <= 0;
+        pwm_duty_cycle <= 0;
+        en <= 0;
+        count = 15;
+    end
     if (en) begin
         data = {data[14:0], sy_copi};
         count = count - 1;
