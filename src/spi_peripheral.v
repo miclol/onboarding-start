1615e6763e3a647944ab86d2b46bc812acb51374
miclol <miclam526@gmail.com>
2025-09-18 01:36:51 -0400

    Bruno

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -11,33 +11,31 @@ module spi_peripheral (
     output reg [7:0] pwm_duty_cycle
 );
 
-reg [1:0] sh_sclk, sh_copi, sh_ncs, sh_rst;
-wire sy_sclk, sy_copi, sy_ncs, sy_rst;
+reg [1:0] sh_sclk, sh_copi, sh_ncs;
+wire sy_sclk, sy_copi, sy_ncs;
 reg en;
 reg [3:0] count = 15;
 reg [15:0] data;
 
-always @(negedge sy_rst) begin
-    en_reg_out_7_0 <= 0;
-    en_reg_out_15_8 <= 0;
-    en_reg_pwm_7_0 <= 0;
-    en_reg_pwm_15_8 <= 0;
-    pwm_duty_cycle <= 0;
-    en <= 0;
-    count = 15;
-end
-
-always @(posedge clk) begin
-    sh_copi <= {sh_copi[0], copi};
-    sh_ncs <= {sh_ncs[0], ncs};
-    sh_sclk <= {sh_sclk[0], sclk};
-    sh_rst <= {sh_rst[0], rst_n};
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        en_reg_out_7_0 <= 0;
+        en_reg_out_15_8 <= 0;
+        en_reg_pwm_7_0 <= 0;
+        en_reg_pwm_15_8 <= 0;
+        pwm_duty_cycle <= 0;
+        en <= 0;
+        count = 15;
+    end else begin
+        sh_copi <= {sh_copi[0], copi};
+        sh_ncs <= {sh_ncs[0], ncs};
+        sh_sclk <= {sh_sclk[0], sclk};
+    end
 end
 
 assign sy_copi = sh_copi[1];
 assign sy_ncs = sh_ncs[1];
 assign sy_sclk = sh_sclk[1];
-assign sy_rst = sh_rst[1];
 
 always @(negedge sy_ncs) begin
     en <= 1;
