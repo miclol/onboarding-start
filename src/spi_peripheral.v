084a932f318be8e15bc281e076afd4427eb5a94f
miclol <miclam526@gmail.com>
2025-09-18 17:05:08 -0400

    Fix gate level mismatch attempt 3

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -13,7 +13,7 @@ module spi_peripheral (
 
 reg [1:0] sh_sclk, sh_copi, sh_ncs;
 wire sy_sclk, sy_copi, sy_ncs;
-reg [3:0] count = 15;
+reg [3:0] count;
 reg [14:0] data;
 
 always @(posedge clk) begin
@@ -26,17 +26,17 @@ assign sy_copi = sh_copi[1];
 assign sy_ncs = sh_ncs[1];
 assign sy_sclk = sh_sclk[1];
 
-always @(posedge sy_sclk) begin
+always @(posedge sy_sclk or negedge rst_n) begin
     if (!rst_n) begin
         en_reg_out_7_0 <= 0;
         en_reg_out_15_8 <= 0;
         en_reg_pwm_7_0 <= 0;
-        en_reg_pwm_15_8 <= 0;
+        en_reg_pwm_15_8 <= 0;  	
         pwm_duty_cycle <= 0;
         count <= 15;
     end else begin
         if (!sy_ncs) begin
-            data <= {data[14:0], sy_copi};
+            data <= {data[13:0], sy_copi};
             count <= count - 1;
             if (count == 0) begin
                 if (data[14] == 1 && data[13:7] <= 4) begin
